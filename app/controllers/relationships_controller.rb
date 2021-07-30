30a002a5af84153fbed4316fdabd456bec63ea23
sumret <sumret@gmail.com>
2021-07-30 02:16:35 +0000

    message for switch following added

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,14 +4,14 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:follow_id])
     current_user.follow(user)
-    flash[:success] = 'ユーザーをフォローしました。'
+    flash[:success] = user.nickname + 'さんをフォローしました。'
     redirect_to user
   end
 
   def destroy
     user = User.find(params[:follow_id])
     current_user.unfollow(user)
-    flash[:success] = 'ユーザーのフォローを解除しました。'
+    flash[:success] = user.nickname + 'さんのフォローをやめました。'
     redirect_to user
 
   end
